37c19b2d1b5098156166474ee1328501bda86132
rraulg5 <rraulg5@gmail.com>
2016-07-15 17:00:00 -0500

    AndroidChat - Binding View Elements with Butterknife

--- GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/LoginActivity.java
+++ GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/LoginActivity.java
@@ -1,13 +1,47 @@
 package mx.raulgarcia.android.androidchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnSignIn)
+    Button btnSignIn;
+    @Bind(R.id.btnSignUp)
+    Button btnSignUp;
+    @Bind(R.id.editTxtEmail)
+    EditText inputEmail;
+    @Bind(R.id.editTxtPassword)
+    EditText inputPassword;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutMainContainer)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignIn)
+    public void handleSignIn() {
+        Log.e("AndroidChat", inputEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignUp)
+    public void handleSignUp() {
     }
 }
