69d7c90c4b81f86c2ea887a797e76766ef61a11d
rraulg5 <rraulg5@gmail.com>
2016-07-20 11:54:52 -0500

    AndroidChat - ContactList MVP Activity

--- GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/contactlist/ui/ContactListActivity.java
+++ GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/contactlist/ui/ContactListActivity.java
@@ -1,15 +1,75 @@
 package mx.raulgarcia.android.androidchat.contactlist.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import mx.raulgarcia.android.androidchat.R;
+import mx.raulgarcia.android.androidchat.contactlist.ContactListPresenter;
+import mx.raulgarcia.android.androidchat.entities.User;
 
-public class ContactListActivity extends AppCompatActivity {
+public class ContactListActivity extends AppCompatActivity implements ContactListView {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.recyclerViewContacts)
+    RecyclerView recyclerViewContacts;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+
+    private ContactListPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        ButterKnife.bind(this);
+
+        presenter.onCreate();
+        toolbar.setTitle(presenter.getCurrentUserEmail());
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        presenter.onPause();
+        super.onPause();
+    }
+
+    @OnClick(R.id.fab)
+    public void addContact() {
+
+    }
+
+    @Override
+    public void onContactAdded(User user) {
+
+    }
+
+    @Override
+    public void onContactChanged(User user) {
+
+    }
+
+    @Override
+    public void onContactRemoved(User user) {
+
     }
 }
