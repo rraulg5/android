213aa2b253479f2c888cff2396c263b2a7a04ebc
rraulg5 <rraulg5@gmail.com>
2016-07-21 11:35:49 -0500

    AndroidChat - ContactList Repository

--- GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/contactlist/ContactListRepositoryImpl.java
+++ GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/contactlist/ContactListRepositoryImpl.java
@@ -1,40 +1,103 @@
 package mx.raulgarcia.android.androidchat.contactlist;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+
+import mx.raulgarcia.android.androidchat.contactlist.events.ContactListEvent;
+import mx.raulgarcia.android.androidchat.domain.FirebaseHelper;
+import mx.raulgarcia.android.androidchat.entities.User;
+import mx.raulgarcia.android.androidchat.lib.EventBus;
+import mx.raulgarcia.android.androidchat.lib.GreenRobotEventBus;
+
 /**
  * Created by rraulg5 on 21/07/2016.
  */
 public class ContactListRepositoryImpl implements ContactListRepository {
 
+    private FirebaseHelper helper;
+    private EventBus eventBus;
+    private ChildEventListener contactEventListener;
 
+    public ContactListRepositoryImpl() {
+        this.eventBus = GreenRobotEventBus.getInstance();
+        this.helper = FirebaseHelper.getInstance();
+    }
 
     @Override
     public void signOff() {
-
+        helper.signoff();
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return null;
+        return helper.getAuthUserEmail();
     }
 
     @Override
     public void removeContact(String email) {
-
+        String currentUserEmail = helper.getAuthUserEmail();
+        helper.getOneContactReference(currentUserEmail, email).removeValue();
+        helper.getOneContactReference(email, currentUserEmail).removeValue();
     }
 
     @Override
     public void destroyListener() {
-
+        contactEventListener = null;
     }
 
     @Override
     public void suscribeToContactListEvents() {
 
+        if (contactEventListener == null) {
+            contactEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactAdded);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactChanged);
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    handleContact(dataSnapshot, ContactListEvent.onContactRemoved);
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {}
+            };
+        }
+        helper.getMyContactsReference().addChildEventListener(contactEventListener);
+    }
+
+    private void handleContact(DataSnapshot dataSnapshot, int type) {
+        String email = dataSnapshot.getKey();
+        email = email.replace("_", ".");
+        boolean online = ((Boolean) dataSnapshot.getValue()).booleanValue();
+        User user = new User();
+        user.setEmail(email);
+        user.setOnline(online);
+        post(type, user);
+    }
+
+    private void post(int type, User user) {
+        ContactListEvent event = new ContactListEvent();
+        event.setEventType(type);
+        event.setUser(user);
+        eventBus.post(event);
     }
 
     @Override
     public void unsuscribeToContactListEvents() {
-
+        if (contactEventListener != null) {
+            helper.getMyContactsReference().removeEventListener(contactEventListener);
+        }
     }
 
     @Override
