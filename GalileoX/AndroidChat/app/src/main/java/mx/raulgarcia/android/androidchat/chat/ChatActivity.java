edc19c47490ddfd931f246f7ef5d435f2b9ea0fa
rraulg5 <rraulg5@gmail.com>
2016-07-22 14:22:55 -0500

    AndroidChat - Chat Layout

---
+++ GalileoX/AndroidChat/app/src/main/java/mx/raulgarcia/android/androidchat/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package mx.raulgarcia.android.androidchat.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.raulgarcia.android.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
